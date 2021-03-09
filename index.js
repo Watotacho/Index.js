76e0a5cf04f0fac2ac6f8c0da17f942588de7b09
Watotacho <watotacho@gmail.com>
2021-03-09 17:52:20 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function fruits(){
+    var fruitList=["mango","kiwi","apple","orange","avocado","pineapple"];
+    for(var i=0;i<fruitList.length;i++){
+        console.log(fruitList[i])
+    }
+
+};
+fruits()
